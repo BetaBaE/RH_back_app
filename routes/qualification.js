d92ace7b2f3324e75b2c011f1c65d13c1baf2c91
yassinethefrt <yassinefartassi01@gmail.com>
2023-06-21 16:10:58 +0100

    remove the qualification delete route

--- routes/qualification.js
+++ routes/qualification.js
@@ -12,7 +12,7 @@ const {
 router.get("/qualification", getQualificationsCount, getQualifications);
 router.post("/qualification", createNewQualification);
 router.get("/qualification/:id", getQualificationById);
-router.delete("/qualification/:id", deleteQualifications);
+// router.delete("/qualification/:id", deleteQualifications);
 router.put("/qualification/:id", updateQualification);
 // router.get("/qualification/count", getQualificationsCount);
 
